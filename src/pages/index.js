d69ddb3fb368fbba0c417349aa23c6961fcec606
velmuruganfrontend <velmuruganfrontenddeveloper@gmail.com>
2019-06-22 12:30:17 +0530

    Initial commit
    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import Head from '../components/head'
+
+const IndexPage = () => {
+    return (
+        <Layout>
+            <Head title="Home" />
+            <h1>Hello.</h1>
+            <h2>I'm Andrew, a full-stack developer living in beautiful Philadelphia.</h2>
+        </Layout>
+    )
+}
+
+export default IndexPage
