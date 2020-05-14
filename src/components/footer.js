fdc4fcafe5274757f88c488156308495bf11e045
velmuruganfrontend <velmuruganfrontenddeveloper@gmail.com>
2020-05-14 10:20:07 +0530

    Updated footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -16,7 +16,7 @@ const Footer = () => {
 
     return (
         <footer className={footerStyles.footer}>
-            <p>Created by {data.site.siteMetadata.author}, © 2019</p>
+            <p>Created by {data.site.siteMetadata.author}, © 2020</p>
         </footer>
     )
 }
